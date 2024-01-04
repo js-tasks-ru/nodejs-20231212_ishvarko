0fb37ded494173d3b728d6a07e4fc49c8887c8d9
Oleksandr Antonenko <118268661+oantonenko-gbsfo@users.noreply.github.com>
2024-01-04 18:58:53 +0200

    05-module

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -8,10 +8,28 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = [];
+
 router.get('/subscribe', async (ctx, next) => {
+    const message = await new Promise((resolve) => {
+        subscribers.push(resolve);
+    });
+    ctx.status = 200;
+    ctx.body = message;
 });
 
 router.post('/publish', async (ctx, next) => {
+    const message = ctx.request.body?.message;
+    if (!message) {
+        ctx.status = 400;
+        return;
+    }
+    for (const resolve of subscribers){
+        resolve(message);
+    };
+    subscribers = []; // Очистка массива
+
+    ctx.status = 200;
 });
 
 app.use(router.routes());
